babeb9d7194ab62d0e3fbcb30c33f4cd27307a9e
VictorOmarT <victoromar001@hotmail.com>
2022-03-21 12:23:55 -0600

    FInish the Media class and the extends

--- index-cards.js
+++ index-cards.js
@@ -1,4 +1,66 @@
-class media{
-    
-    
+class media {
+    constructor(title) {
+        this._title = title;
+        this._isChekedout = false;
+        this._ratings = [];
+    }
+
+    get title() {
+        return this._title;
+    }
+    get isChekedout() {
+        return this._isChekedout;
+    }
+    get rating() {
+        return this._ratings;
+    }
+    set isChekedout(value) {
+        this._isChekedout = value;
+    }
+
+    tooggleChekedout() {
+        if (this._isChekedout == true) {
+            this._isChekedout = false;
+        } else {
+            this._isChekedout = true;
+        }
+    }
+
+    getAverageRating() {
+        let sumRating = this._ratings.reduce((i, rating) => i + rating);
+        return sumRating / this._ratings.length;
+    }
+
+    addRating(value) {
+        this._ratings.push(value);
+    }
+
 }
+
+class Book extends media {
+
+    constructor(author, title, pages) {
+        super(title);
+        this._author = author;
+        this._pages = pages;
+    }
+    get author() { return this._author; }
+    get title() { return this._author}
+}
+
+class Movie extends media {
+    
+    constructor(director, title, runTime){
+        super(title);
+        this._director = director;
+        this._runTime = runTime;
+    }
+
+    get director() { return this._director; }
+    get runTime() { return this._runTime; }
+}
+
+const historyOfEverything = new Book('Bill Bryson', 'A short story of  Nerly Everything', 544);
+
+historyOfEverything.tooggleChekedout();
+console.log(historyOfEverything._isChekedout);
